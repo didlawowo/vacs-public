b7757487c9489694a58bad96be63fe7c4d4313dd
Mark Edmondson <github@markedmondson.me>
2024-05-03 13:48:56 +0200

    Update main.py

--- webapp/cl_events/main.py
+++ webapp/cl_events/main.py
@@ -1,42 +1,9 @@
 import chainlit as cl
 import logging
 from sunholo.agents import send_to_qa_async
-from sunholo.logging import setup_logging
 from sunholo.streaming import generate_proxy_stream_async
 from .config import lookup_config
 import json
-log = setup_logging(logger_name="chainlit")
-
-logger_httpx = logging.getLogger('httpx')
-
-# Set the logging level to WARNING, so INFO messages will be suppressed
-logger_httpx.setLevel(logging.WARNING)
-
-def generate_chainlit_output(bot_output):
-    # just pass through as processes after stream the JSON object
-    return bot_output
-
-def generate_chainlit_sources(bot_output):
-
-    try:
-        the_json = json.loads(bot_output)
-    except Exception as e:
-        log.warning("Could not parse chainlit output {bot_output}")
-        return None
-    
-    source_documents = the_json.get("source_documents", None)  # type: List[Document]
-
-    text_elements = ""  # type: List[cl.Text]
-
-    if source_documents:
-        for source_idx, source_doc in enumerate(source_documents):
-            source_metadata = source_doc.get("metadata")
-            log.info(f"source_metadata: {source_metadata}")
-            source_name = source_metadata.get("source")
-            # Create the text element referenced in the message
-            text_elements += f"\n * {source_name}"
-    
-    return text_elements
 
 async def main_logic(message: cl.Message):
     app_user = cl.user_session.get("user")
@@ -89,17 +56,9 @@ async def main_logic(message: cl.Message):
                 token_str = token.decode('utf-8')  # Decoding byte string to a regular string
             else:
                 token_str = token  # It's already a string
-
-            if token_str.startswith("###JSON"):
-                json_part = token_str.split("###JSON_START###")[1].split("###JSON_END###")[0]
-                log.info(f"Got JSON part {json_part}")
-                txt_elements = generate_chainlit_sources(json_part)
-                if txt_elements:
-                    await msg.stream_token(f"\n\n*Sources:*\n{txt_elements}")
-            else:
-                await msg.stream_token(token_str)
+            await msg.stream_token(token_str)
+            
     log.info("chanlit streaming finished")
-
 
     # Send the final message
     memory.append({"name": "AI", "content": msg.content})  
